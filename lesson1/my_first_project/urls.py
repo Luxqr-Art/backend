7289d6c3c94d0253b788e716e9ebc8fd9a5cb8ab
Luxqr <231196pop>
2020-08-18 14:42:47 +0300

    rename folder

--- lesson1/my_first_project/urls.py
+++ lesson1/my_first_project/urls.py
@@ -1,4 +1,4 @@
-"""mt_first_project URL Configuration
+"""my_first_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- lesson1/my_first_project/wsgi.py
+++ lesson1/my_first_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mt_first_project project.
+WSGI config for my_first_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mt_first_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_first_project.settings')
 
 application = get_wsgi_application()

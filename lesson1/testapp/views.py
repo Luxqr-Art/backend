acd604b871ccaf2ff902ef4ee5c0adce335f50fc
Luxqr <231196pop>
2020-08-19 05:57:43 +0300

    edit commit v 1.1

--- lesson1/testapp/views.py
+++ lesson1/testapp/views.py
@@ -2,44 +2,24 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.http import JsonResponse
 
-
+comments = [
+        {'first_name': 'Steve',
+         'last_name': 'Jobs',
+         'adress': '775 Westminster Avenue APT D5'},
+        {'first_name': 'Bill',
+         'last_name': 'Gates',
+         'adress': ' 700 Carpenters Crossing APT D12'}
+        ]
 def welcome(request):
     return render(request, 'index1.html')
 
 def json(request):
-    comments = [
-        {'first_name': 'Steve',
-         'last_name': 'Jobs',
-         'adress': '775 Westminster Avenue APT D5'},
-        {'first_name': 'Bill',
-         'last_name': 'Gates',
-         'adress': ' 700 Carpenters Crossing APT D12'}
-        ]
-
-    return JsonResponse({'comments' : comments})
-
+    return JsonResponse(comments, safe=False)
 
 def resp(request):
-    comments = [
-        {'first_name': 'Steve',
-         'last_name': 'Jobs',
-         'adress': '775 Westminster Avenue APT D5'},
-        {'first_name': 'Bill',
-         'last_name': 'Gates',
-         'adress': ' 700 Carpenters Crossing APT D12'}
-        ]
-    return HttpResponse(comments)
+   return HttpResponse(comments, content_type='application/json')
 
 def html(request):
-    comments = [
-        {'first_name': 'Steve',
-         'last_name': 'Jobs',
-         'adress': '775 Westminster Avenue APT D5'},
-        {'first_name': 'Bill',
-         'last_name': 'Gates',
-         'adress': ' 700 Carpenters Crossing APT D12'}
-        ]
-
     context = {'comments' : comments}
 
     return render(request, 'index.html', context)
